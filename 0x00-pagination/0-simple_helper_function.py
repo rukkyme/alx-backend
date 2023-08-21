cd2659f3eb8f117f41de430aa3056b9fc2d8dc55
rukkyme <communicatejoy@yhaoo.com>
2023-08-21 17:21:49 +0100

    pagination unfolds

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+function named index_range that takes two integer arguments 
+page and page_size
+"""
+def index_range(page: int, page_size: int):
+    """Generate list index range"""
+    start_index(page - 1) * page_size
+    end_index(start_index + end_index)
+    return start_index, end_index
