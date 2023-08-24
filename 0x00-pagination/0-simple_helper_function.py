64015294931d1f5e3642adaaddc7aaf70baf6c22
rukkyme <communicatejoy@yhaoo.com>
2023-08-24 18:58:19 +0100

    pagination it is!

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 """
-function named index_range that takes two integer arguments 
+function named index_range that takes two integer arguments
 page and page_size
 """
+
+
 def index_range(page: int, page_size: int):
     """Generate list index range"""
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
-
